3236cef8de4619ef755a36305250709ad0d79bd8
Kazuki Suda <kazuki.suda@gmail.com>
2020-03-04 23:08:43 +0900

    wip

---
+++ __tests__/installer.test.ts
@@ -0,0 +1,9 @@
+import * as installer from "../src/installer";
+
+describe("installer", () => {
+  describe("download", () => {
+    it("can download a tool", async () => {
+      // TODO
+    });
+  });
+});
--- src/installer.ts
+++ src/installer.ts
@@ -2,13 +2,21 @@ import * as core from "@actions/core";
 import * as tc from "@actions/tool-cache";
 import * as filenamifyUrl from "filenamify-url";
 
-export const download = async (url: string, toolName: string): Promise<string> => {
+export const download = async (
+  url: string,
+  toolName: string
+): Promise<string> => {
   core.info(`Downloading from ${url}`);
 
   const downloadPath: string = await tc.downloadTool(url);
-  core.debug(`Downloaded to ${downloadPath}`)
+  core.debug(`Downloaded to ${downloadPath}`);
 
-  const cachePath: string = await tc.cacheFile(downloadPath, toolName, toolName, filenamifyUrl(url));
+  const cachePath: string = await tc.cacheFile(
+    downloadPath,
+    toolName,
+    toolName,
+    filenamifyUrl(url)
+  );
   core.info(`Cached ${downloadPath} to ${cachePath}`);
 
   return cachePath;
